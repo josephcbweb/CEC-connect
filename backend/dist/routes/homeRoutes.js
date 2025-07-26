de03891302d197a54fd08861f1569bde3af5a47c
Joseph C B <josephcb581@gmail.com>
2025-07-26 18:17:26 +0530

    clean up vite files create auth component

--- backend/dist/routes/homeRoutes.js
+++ backend/dist/routes/homeRoutes.js
@@ -6,6 +6,6 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const router = express_1.default.Router();
 router.get("/", (req, res) => {
-    res.send("This is the homepage our app...");
+    res.send("This is the homepage our app. Wohoo...");
 });
 exports.default = router;
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,11 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <div className="bg-amber-400 h-screen w-full flex text-3xl justify-center items-center">
+      Hlooo
+    </div>
+  </StrictMode>
+);
