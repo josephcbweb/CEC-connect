02b4d5e343120159ca6ee91898e90bb397dab4aa
Sreenath V <sreenathvenugopal0465@gmail.com>
2025-09-28 16:21:14 +0530

    Design sidebar for admin dashboard

--- frontend/src/components/admin/Home.tsx
+++ frontend/src/components/admin/Home.tsx
@@ -2,9 +2,9 @@ const Home = () => {
   return (
     <div>
       <h1 className="text-3xl font-bold text-slate-800">Dashboard</h1>
-      <p className="mt-2 text-gray-600">Welcome to your Acads project!</p>
+      <p className="mt-2 text-[#3AA9AB] text-2xl font-semibold">Welcome!</p>
 
-      <div className="mt-8 p-6 bg-white rounded-lg shadow-md">
+      <div className="mt-8 p-6 bg-gray-100 rounded-lg shadow-md">
         <h2 className="text-xl font-semibold">Your Content Area</h2>
         <p className="mt-2">
           This is where the main content for each page will be displayed. The
