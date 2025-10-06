e0b87e16bb58a8e33b0a883ae71c9b8731dfbf38
bmeennakshy <bmeennakshy321@gmail.com>
2025-10-06 20:37:07 +0530

    Generate certificate template

--- backend/server.ts
+++ backend/server.ts
@@ -4,11 +4,8 @@ import homeRoutes from "./routes/homeRoutes";
 import adminRoutes from "./routes/adminRoutes";
 import studentRoutes from "./routes/studentRoutes";
 import feeRoutes from "./routes/feeRoutes";
-<<<<<<< HEAD
 import certificateRoutes from './routes/certificateRoutes';
-=======
 import authRouter from "./routes/authRouter";
->>>>>>> 3db0e6507ee6b533567e050507891dd314d8468c
 import cors from "cors";
 dotenv.config();
 
@@ -22,11 +19,8 @@ app.use("/admin",adminRoutes);
 
 app.use("/students", studentRoutes);
 app.use("/fee", feeRoutes);
-<<<<<<< HEAD
 app.use("/api", certificateRoutes);
-=======
 app.use("/auth", authRouter);
->>>>>>> 3db0e6507ee6b533567e050507891dd314d8468c
 app.listen(PORT, () => {
   console.log("Server listening on port: ", PORT);
 });
