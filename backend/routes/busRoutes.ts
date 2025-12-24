f32272aac43c0013e0dbcfc04dc21629412f5f7c
Sreenath V <sreenathvenugopal0465@gmail.com>
2025-12-24 16:25:43 +0530

    Implement student promotion functionality

--- backend/routes/busRoutes.ts
+++ backend/routes/busRoutes.ts
@@ -1,6 +1,6 @@
 import express from "express";
 import { Router } from "express";
-import {fetchBus, addBus, getBusDetails,addBusStops,deleteBusStop } from "../controllers/busController";
+import {fetchBus, addBus, getBusDetails,addBusStops,deleteBusStop,getBusFeeStatus,toggleBusFee } from "../controllers/busController";
 
 const router = express.Router();
 
@@ -9,4 +9,7 @@ router.post("/addBus", addBus);//route for adding a new bus.
 router.get("/busDetails/:busId",getBusDetails);
 router.post("/addStop",addBusStops);
 router.delete("/deleteStop/:id",deleteBusStop);
+router.get("/busFeeStatus", getBusFeeStatus);//returns whether the admin has turned on the bus fees or not.
+router.put("/toggleBusFee", toggleBusFee);//used to change the assignBusFee status to true or false
+
 export default router;
